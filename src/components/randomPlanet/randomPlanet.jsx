fed6b1c9a5af8bacc154a18069ed20facb51f598
Alexander Kleymonov <Kleymonov.Alexander@gmail.com>
2019-07-01 14:16:55 +0300

    Created random planets.

--- src/components/randomPlanet/randomPlanet.jsx
+++ src/components/randomPlanet/randomPlanet.jsx
@@ -1,39 +1,37 @@
 import React, { Component } from 'react';
+import SwapiService from '../../services/SwapiService';
+import Spinner from '../spinner';
 
+import PlanetView from './planetView';
 import './randomPlanet.css';
 
 export default class RandomPlanet extends Component {
+  swapiService = new SwapiService();
+
   constructor() {
     super();
-    this.state = {};
+
+    this.state = {
+      planet: {},
+      loading: true,
+    };
+
+    this.updatePlanet();
+  }
+
+  onPlanetLoad = (planet) => {
+    this.setState({ planet, loading: false });
+  };
+
+  updatePlanet() {
+    const id = Math.floor(Math.random() * 15 + 2);
+    this.swapiService.getPlanet(id).then(this.onPlanetLoad);
   }
 
   render() {
-    return (
-      <div className="random-planet jumbotron rounded">
-        <img
-          className="planet-image"
-          src="https://starwars-visualguide.com/assets/img/planets/5.jpg"
-          alt="planet"
-        />
-        <div className="random-planet-desc">
-          <h4>Planet Name</h4>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <span className="term">Population</span>
-              <span>123124</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Rotation Period</span>
-              <span>43</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Diameter</span>
-              <span>100</span>
-            </li>
-          </ul>
-        </div>
-      </div>
-    );
+    const { planet, loading } = this.state;
+    const content = loading ? <Spinner /> : <PlanetView planet={planet} />;
+
+    return <div className="random-planet jumbotron rounded">{content}</div>;
   }
 }
---
+++ src/components/spinner/spinner.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import './spinner.css';
+
+const Spinner = () => (
+  <div className="lds-css ng-scope">
+    <div className="lds-spin">
+      <div>
+        <div />
+      </div>
+      <div>
+        <div />
+      </div>
+      <div>
+        <div />
+      </div>
+      <div>
+        <div />
+      </div>
+      <div>
+        <div />
+      </div>
+      <div>
+        <div />
+      </div>
+      <div>
+        <div />
+      </div>
+      <div>
+        <div />
+      </div>
+    </div>
+  </div>
+);
+
+export default Spinner;
